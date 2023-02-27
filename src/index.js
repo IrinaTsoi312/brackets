f8ea36691105a5156d15b2278f2925f2f7bcd925
IrinaTsoi312 <irinatsoi312@gmail.com>
2023-02-27 22:32:49 +0600

    feat: create check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pattern = [];
+  const stack = [];
+  let str2;
+  let isCorrect = false;
+
+  function findString(string) {
+    string.forEach(item => {
+      if(typeof item === "string") {
+        pattern.push(item);
+      } 
+      else {
+        findString(item);
+      }
+    });
+  }
+  findString(bracketsConfig);
+
+  if(pattern.join("").includes("|") && pattern.length === 2) {
+    const arr1 = pattern.filter(item => item === "|");
+    const arr2 = str.split("").filter(item => item === "|");
+    arr1.length === arr2.length ? isCorrect = true : isCorrect = false;
+  }
+  else if(pattern.join("").includes("|") && pattern.length !== 2) {
+    if(pattern.join("").includes("|")) {
+      str2 = str.split("").filter(item => item !== "|").join("");
+      getIsCorrect(str2);
+    }
+  }
+  else {
+    getIsCorrect(str);
+  }
+  function getIsCorrect(str) {
+    for(let char of str) {
+      let index = pattern.indexOf(char);
+      if(index % 2 === 0) { stack.push(index + 1);} 
+      else { if(stack.pop() !== index) { return false; } }
+    }
+  }
+  stack.length === 0 ? isCorrect = true : isCorrect = false;
+  return isCorrect;
 }
